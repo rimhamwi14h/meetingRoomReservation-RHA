50145507a6ab6343e33a87d5a1f010a93ba6eb80
rimhamwi14 <rimhamwi14@gmail.com>
2026-09-11 17:30:28 +0200

    Add Building service

---
+++ src/main/java/roomreservation/service/BuildingService.java
@@ -0,0 +1,30 @@
+package roomreservation.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import roomreservation.model.Building;
+import roomreservation.repository.BuildingRepository;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public class BuildingService {
+    @Autowired
+    private BuildingRepository buildingRepository;
+    public Building createBuilding(Building building){
+        return buildingRepository.save(building);
+    }
+    public List<Building> getAllBuildings(){
+        List<Building> buildings =buildingRepository.findAll();
+        buildings.sort(
+                Comparator.comparing(
+                        Building::getName,
+                        String.CASE_INSENSITIVE_ORDER
+                )
+        );
+        return buildings;
+    }
+    public Building getBuilding (Long id){
+        return buildingRepository.findById(id).orElse(null);
+    }
+}
